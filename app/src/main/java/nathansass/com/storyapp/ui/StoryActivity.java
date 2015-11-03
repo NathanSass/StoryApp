4837f28559be5b07d7e5055431b4fba46f3cec00
Nathan Sass <nsass711@gmail.com>
2015-11-03 15:18:30 -0800

    pages are dynamic and the app restarts

--- app/src/main/java/nathansass/com/storyapp/ui/StoryActivity.java
+++ app/src/main/java/nathansass/com/storyapp/ui/StoryActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +26,7 @@ public class StoryActivity extends AppCompatActivity {
     private Button mChoice1;
     private Button mChoice2;
     private String mName;
+    private Page mCurrentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +45,49 @@ public class StoryActivity extends AppCompatActivity {
         mChoice1 = (Button) findViewById(R.id.choiceButton1);
         mChoice2 = (Button) findViewById((R.id.choiceButton2));
 
-        loadPage();
+        loadPage(0);
     }
 
-    private void loadPage() {
-        Page page = mStory.getPage(0);
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
 
-        Drawable drawable =  ResourcesCompat.getDrawable(getResources(), page.getImageId(), null);
+        Drawable drawable =  ResourcesCompat.getDrawable(getResources(), mCurrentPage.getImageId(), null);
 
-        String pageText = page.getText();
+        String pageText = mCurrentPage.getText();
         pageText = String.format(pageText, mName);
         mImageView.setImageDrawable(drawable);
 
         mTextView.setText(pageText);
-        mChoice1.setText(page.getChoice1().getText());
-        mChoice2.setText(page.getChoice2().getText());
+        if(mCurrentPage.isFinal()) {
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+            mChoice2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } else {
+
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+        }
     }
 
 }
